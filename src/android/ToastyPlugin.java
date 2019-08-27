e9a14adf012bdbbb152d736f6978ebd2da5c57e3
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-08-27 12:58:29 -0500

    Update ToastyPlugin.java

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -23,7 +23,18 @@ public class ToastyPlugin extends CordovaPlugin {
       return false;
     }else{
       
-      callToast();
+      String message;
+      String duration;
+      try {
+        JSONObject options = args.getJSONObject(0);
+        message = options.getString("message");
+        duration = options.getString("duration");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
+      
+      callToast(message);
       
       PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
       callbackContext.sendPluginResult(pluginResult);
@@ -32,9 +43,9 @@ public class ToastyPlugin extends CordovaPlugin {
      
   }
   
-  public void callToast(){
+  public void callToast(String valor){
   
-    Toast toast1 = Toast.makeText(cordova.getActivity(), "Call BackPressedPlugin ! ",Toast.LENGTH_SHORT);
+    Toast toast1 = Toast.makeText(cordova.getActivity(), "Call BackPressedPlugin ! + valor",Toast.LENGTH_SHORT);
       toast1.show();
   
   }
