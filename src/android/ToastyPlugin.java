c6b93c46d8ba43fe14aca2bd0d48416167b18ec0
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-08-28 18:25:15 -0500

    Update ToastyPlugin.java

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -44,7 +44,7 @@ public class ToastyPlugin extends CordovaPlugin {
 	
   public void callToast(String valor){
 	  
-	doubleBackToExitPressedOnce=true
+	//doubleBackToExitPressedOnce=true
 	//Toast toast1 = Toast.makeText(cordova.getActivity(), valor + doubleBackToExitPressedOnce ,Toast.LENGTH_SHORT);
 	//toast1.show();
 	  
